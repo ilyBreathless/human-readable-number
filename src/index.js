5df6bffbb6d59b076e4ac889ecd2f00b82809cdc
ilyBreathless <72036113+ilyBreathless@users.noreply.github.com>
2021-03-14 14:27:03 +0300

    feat : add human readable number solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
+  module.exports = function toReadable (number) {
+    let arrFrom1To19 = [' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen']
+    let arrTens = [' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety']
+    let res = []
+    let hundreds = Math.floor(number / 100)
+    let tens = Math.floor(number % 100 / 10)
+    let ones = number % 10
+
+    if (number == 0) {
+        return ('zero')
+    }
+    if(hundreds != 0) {
+        res.push(arrFrom1To19[hundreds-1] + ' hundred')
+    }
+
+    if(tens >= 2) {
+        res.push(arrTens[tens - 2])
+        res.push(arrFrom1To19[ones - 1])
+    } else {
+        res.push(arrFrom1To19[number%100 - 1])
+    }
+    return res.join('').trim()
+}
+
 }
